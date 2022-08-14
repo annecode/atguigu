1befda3e9c32542799311bbb2c209c00d3fc9f32
annecode <976413691@qq.com>
2022-08-14 18:34:12 +0800

    2

--- chapter01/part3/src/index.ts
+++ chapter01/part3/src/index.ts
@@ -1,9 +1,3 @@
-/*
- * @Author: anne.yang
- * @Date: 2022-08-06 18:51:46
- * @LastEditors: anne.yang
- * @LastEditTime: 2022-08-06 20:14:28
- */
 import {hi} from './m1';
 
 function sum(a: number, b: number):number{
@@ -18,7 +12,7 @@ obj.age = 18;
 console.log(obj);
 
 console.log(sum(123, 456));
-console.log('哈哈好的');
+console.log('哈哈');
 console.log(hi);
 
 let fn = (a: number, b: number) => a + b;
